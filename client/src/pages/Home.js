94c705b44762e9885c02ee8c378989767e2ba399
wa20 <78425538+wa20@users.noreply.github.com>
2022-01-16 13:44:12 +0000

    header component added to home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import {Navbar, Header, Loader, Services, Transactions, Footer} from '../components/index'
+
+
+const Home = () => {
+
+    return (
+         <div className='min-h-screen'>
+             <div className=''>
+             <Navbar/>
+             </div>
+
+
+        </div>
+    )
+    
+    } 
+     
+    export default Home;
