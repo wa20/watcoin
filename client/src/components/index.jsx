0d736e89af8f42d8b3962a139efe4d66cb005955
wa20 <78425538+wa20@users.noreply.github.com>
2022-01-21 14:40:11 +0000

    import react from react added to componenets

--- client/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Navbar} from "./Navbar"
-export { default as Header} from "./Header"
-export { default as Footer} from "./Footer"
-export { default as Loader} from "./Loader"
-export { default as Services} from "./Services"
-export { default as Transactions} from "./Transactions"
